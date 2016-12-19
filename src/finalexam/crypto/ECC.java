3fe9d744eae45381ae539314cc7482a162a55918
brragul <br.ragul@gmail.com>
2016-12-18 21:47:34 -0500

    order,2p,p+q,xP

--- src/finalexam/crypto/ECC.java
+++ src/finalexam/crypto/ECC.java
@@ -1,6 +1,8 @@
 package finalexam.crypto;
 
 import java.math.BigInteger;
+import java.util.Stack;
+
 import midterm.exam.Init;
 
 public class ECC {
@@ -9,13 +11,27 @@ public class ECC {
 		ECC ecc = new ECC();
 		//ecc.twoP(new Point(BigInteger.valueOf(22),BigInteger.valueOf(12)), BigInteger.valueOf(23), BigInteger.valueOf(2));
 		//ecc.addPoints(new Point(BigInteger.valueOf(0),BigInteger.valueOf(3)),new Point(BigInteger.valueOf(13),BigInteger.valueOf(1)), BigInteger.valueOf(23), BigInteger.valueOf(2));
-		ecc.findOrder(new Point(BigInteger.valueOf(2288),BigInteger.valueOf(1585)), BigInteger.valueOf(3571), BigInteger.valueOf(7));
+		//ecc.findOrder(new Point(BigInteger.valueOf(2288),BigInteger.valueOf(1585)), BigInteger.valueOf(3571), BigInteger.valueOf(7));
+		//ecc.findXP(150, new Point(BigInteger.valueOf(16),BigInteger.valueOf(3096)), BigInteger.valueOf(3571), BigInteger.valueOf(7));
 	}
-	public int findXP(int X,Point P,BigInteger p){
-		
-		
-		
-		return 0;
+	
+	//Find X times P using Double and Add algorithm.
+	public Point findXP(int X,Point P,BigInteger p,BigInteger a){
+		Point tP = P;
+		String s = Integer.toBinaryString(X);
+		int t=1,k=2;
+		while(t<X){
+			P = twoP(P, p, a);
+			t +=t;
+			if(!Integer.toBinaryString(t).equals(Integer.toBinaryString(X).substring(0, k))){
+				t++;
+				P=addPoints(tP, P, p, a);
+			}
+			k++;
+		}
+		System.out.println("12P");
+		P.printPoint();
+		return P;
 	}
 	
 	public int findOrder(Point P,BigInteger p,BigInteger a){
@@ -44,6 +60,7 @@ public class ECC {
 		Point p2 = new Point();
 		p2.x = (((alpha.pow(2)).subtract(P.x)).subtract(Q.x)).mod(p);
 		p2.y = ((alpha.multiply(P.x.subtract(p2.x))).subtract(P.y)).mod(p);
+		System.out.println("P+Q");
 		p2.printPoint();
 		return p2;
 	}
@@ -55,6 +72,7 @@ public class ECC {
 		Point p2 = new Point();
 		p2.x = ((alpha.pow(2)).subtract(P.x.multiply(BigInteger.valueOf(2)))).mod(p);
 		p2.y = ((alpha.multiply(P.x.subtract(p2.x))).subtract(P.y)).mod(p);
+		System.out.println("2P");
 		p2.printPoint();
 		return p2;
 	}
